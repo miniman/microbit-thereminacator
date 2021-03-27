d0d62c2d616c8236f3e0f1e90382675eb2266231
Rusty Weise <miniman@users.noreply.github.com>
2021-03-27 19:49:26 +0000

    Update thereminicator.js
    Pressing buttons A or B will now change the base note each thereminicator is emitting in a loop from low -> high with button A and vice versa. A README.md is forthcoming.

--- thereminicator.js
+++ thereminicator.js
@@ -1,14 +1,32 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    radio.setGroup(1)
+    bargraph_level = 128 + radio.receivedPacket(RadioPacketProperty.SignalStrength)
     led.plotBarGraph(
-    Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength)) - 42,
-    128
+    bargraph_level,
+    86
     )
-    music.playTone(Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength)) - 42, music.beat(BeatFraction.Sixteenth))
+    playing_note = radio.receivedPacket(RadioPacketProperty.SignalStrength) + notes[current_note]
+    music.ringTone(playing_note)
 })
+input.onButtonPressed(Button.A, function () {
+    if (current_note == notes.length - 1) {
+        current_note = 0
+    } else {
+        current_note += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (current_note == 0) {
+        current_note = notes.length - 1
+    } else {
+        current_note = current_note - 1
+    }
+})
+let current_note = 0
+let playing_note = 0
+let bargraph_level = 0
+let notes: number[] = []
+notes = [261, 293, 329, 349, 392, 440, 493]
 basic.forever(function () {
-    radio.setGroup(1)
-    radio.sendNumber(0)
-    radio.setTransmitPower(3)
+    radio.sendNumber(1)
+    radio.setTransmitPower(1)
 })
-
